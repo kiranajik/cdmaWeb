121511884bfc38675a87f1c4b7c7991d184000a6
kiranajik <kiranlcw31.anair@gmail.com>
2021-06-23 21:34:07 +0530

    vaccination analysis added

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" type="text/css" href="nav.css">
 </head>
 <div class="nav-bar">
-    <a href="#" class="nav-button">Covid Cases</a>
-    <a href="#" class="nav-button">Vaccination</a>
-    <a href="#" class="nav-button">Relief Fund</a>
+    <a href="index.php" class="nav-button">Covid Cases</a>
+    <a href="vaccination.php" class="nav-button">Vaccination</a>
+    <a href="doantion.php" class="nav-button">Relief Fund</a>
 </div>
---
+++ vaccination.php
@@ -0,0 +1,394 @@
+<?php
+
+
+$conn = new mysqli("localhost","root","","covid");
+// $conn = new mysqli("localhost","u307892079_kiranajik","Kiran@#123","u307892079_covid");
+
+
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $db->connect_error;
+    exit();
+}
+
+$sql1 = "SELECT vDistrict, COUNT(*) AS freq FROM vaccine GROUP BY vDistrict";
+$sql2 = "SELECT vGender, COUNT(*) AS freq FROM vaccine GROUP BY vGender";
+$sql3 = "SELECT vAge, COUNT(*) AS freq FROM vaccine GROUP BY vAge";
+$sql4 = "SELECT vPrecond, COUNT(*) AS freq FROM vaccine GROUP BY vPrecond";
+$sql5a = "SELECT vDistrict, COUNT(*) AS freq FROM vaccine WHERE vGender='M' GROUP BY vDistrict";
+$sql5b = "SELECT vDistrict, COUNT(*) AS freq FROM vaccine WHERE vGender='F' GROUP BY vDistrict";
+
+
+$result1 = $conn->query($sql1);
+$result2 = $conn->query($sql2);
+$result3 = $conn->query($sql3);
+$result4 = $conn->query($sql4);
+$result5a = $conn->query($sql5a);
+$result5b = $conn->query($sql5b);
+
+$districtLabels=[];
+$districtFrequecy=[];
+$genderLabel=[];
+$genderFrequency=[];
+$ageLabels=[];
+$ageFrequency=[];
+$precondLabels=[];
+$precondFrequency=[];
+$maleDistrictFrequency=[];
+$femaleDistrictFrequency=[];
+
+if ($result1->num_rows > 0) {
+    while($row = $result1->fetch_assoc()) {
+      $districtLabels[] = $row["vDistrict"];
+      $districtFrequecy[]= $row["freq"];
+    }
+} 
+else 
+{
+    echo "0 results";
+}
+
+
+if ($result2->num_rows > 0) {
+    while($row = $result2->fetch_assoc()) {
+      $genderLabels[] = $row["vGender"];
+      $genderFrequency[] = $row["freq"];
+    }
+} 
+
+if ($result3->num_rows > 0) {
+    while($row = $result3->fetch_assoc()) {
+      $ageLabels[] = $row["vAge"];
+      $ageFrequency[] = $row["freq"];
+    }
+  } 
+
+if ($result4->num_rows > 0) {
+    while($row = $result4->fetch_assoc()) {
+      $precondLabels[] = $row["vPrecond"];
+      $precondFrequency[] = $row["freq"];
+    }
+} 
+
+if ($result5a->num_rows > 0) {
+  while($row = $result5a->fetch_assoc()) {
+    $maleDistrictFrequency[] = $row["freq"];
+  }
+} 
+if ($result5b->num_rows > 0) {
+  while($row = $result5b->fetch_assoc()) {
+    $femaleDistrictFrequency[] = $row["freq"];
+  }
+} 
+$conn->close();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CDMA</title>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+  <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+          }
+
+        body{
+          background-color:#e2e1e0;
+        }
+
+        .dark{
+          background-color:#121212;
+        }
+
+
+        @media screen and (min-width: 650px) {
+      .container {
+        width:60vw;
+        height:60vh;
+        margin: 0 auto;
+        
+      }
+              
+    
+      .space{
+      height:2em !important;
+      }
+    }
+     
+     
+     
+     
+      @media screen and (max-width: 650px) {
+      .container {
+        width:87vw;
+        margin: 0 auto;
+        
+      }
+      .space{
+      height:1em;
+    }
+ }
+
+    .item{
+      background-color:white;
+      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+      padding:20px;
+      border-radius:10px;
+    }
+
+    .item-dark{
+      background-color:#1f1f1f;
+     }
+    .space{
+      height:1em;
+    }
+    /* .container{
+      margin-top:2em;
+    } */
+
+</style>
+</head>
+<body>
+
+<?php include "header.php"; ?>
+<?php include "nav.php"; ?>
+<div class="container">
+
+
+    <section class="covid-cases">
+        <div class="item">
+        <canvas id="histogram"></canvas>
+        </div>
+        <div class="space"></div>
+        <div class="item">
+        <canvas id="myChart"></canvas>
+        </div>
+        <div class="space"></div>
+        <div class="item">
+        <canvas id="line-chart"></canvas>
+        </div>
+        <div class="space"></div>
+        <div class="item">
+        <canvas id="bar-chart-horizontal" width="800" height="450"></canvas>
+        </div>
+        <div class="space"></div>
+        <div class="item">
+        <canvas id="district-gender-chart" width="800" height="450"></canvas>
+        </div>
+
+
+
+    </section>
+</div>
+
+<script>
+
+
+const ctx = document.getElementById('histogram').getContext('2d');
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels:["ALP","EKM","IDK","KNR","KGD","KLM","KTM","KKD","MLP","PKD","PT","TVM","TSR","WAD"],
+    datasets: [{
+      label: 'Vaccinations District-Wise  Distribution',
+      data: <?php echo json_encode($districtFrequecy); ?>,
+      backgroundColor: [
+
+      '#F54748',
+      '#ff96ad',
+      '#005a8d',
+      '#022e57',
+      "#95a5a6",
+        "#2b59b6",
+        "#f1c40f",
+        "#fe9c8f",
+        "#34495e",
+        "#851e3e",
+        "#63ace5",
+        "#009688",
+        "#96ceb4",
+        "#76b4bd"
+      ],
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        display: false,
+        barPercentage: 1.0,
+        ticks: {
+          max: 3,
+        }
+      }, 
+      {
+        display: true,
+        ticks: {
+          autoSkip: false,
+          max: 4,
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          autoskip:true
+        }
+      }]
+    },
+    legend :  { display: false },
+    title: {
+        display: true,
+        text: 'Total covid Cases - District Wise Distribution',
+        fontSize:10
+      }
+  },
+});
+
+var btx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(btx, {
+  type: 'pie',
+  data: {
+    labels: ["Male","Female"],
+    datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#3498db"
+      ],
+      data: <?php echo json_encode($genderFrequency); ?>,
+    }]
+  },
+});
+
+var dtx = document.getElementById("line-chart").getContext('2d');
+var lineChart = new Chart(dtx, {
+  type: 'line',
+  data: {
+    labels: ["0-10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100", "100+"],
+    datasets: [
+      {
+        label: "Vaccinations Age Wise Distribution",
+        data: <?php echo json_encode($ageFrequency); ?>,
+        backgroundColor:'#F54748',
+        borderColor: 'rgb(75, 192, 192)'
+      }
+    ],
+    options: {
+  	scales: {
+    	yAxes: [{
+        ticks: {
+					reverse: false
+        }
+      }]
+    },
+
+  }
+  }
+});
+
+
+new Chart(document.getElementById("bar-chart-horizontal"), {
+    type: 'horizontalBar',
+    data: {
+      labels: <?php echo json_encode($precondLabels); ?>,
+      datasets: [
+        {
+          label: "Vaccinations Done",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", "#95a5a6",
+        "#2b59b6",
+        "#f1c40f",
+        "#fe9c8f",],
+          data: <?php echo json_encode($precondFrequency); ?>
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Preconditions of Vaccinated'
+      }
+    }
+});
+
+new Chart(document.getElementById("district-gender-chart"), {
+  type: 'line',
+  data: {
+    labels: ["ALP","EKM","IDK","KNR","KGD","KLM","KTM","KKD","MLP","PKD","PT","TVM","TSR","WAD"],
+    datasets: [{ 
+        data: <?php echo json_encode($maleDistrictFrequency);?>,
+        label: "Male",
+        borderColor: "#3e95cd",
+        backgroundColor:"#3e95cd",
+        fill: false
+      }, 
+      { 
+        data:  <?php echo json_encode($femaleDistrictFrequency);?>,
+        label: "Female",
+        borderColor: "#3cba9f",
+        backgroundColor:"#3cba9f",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'District Wise Gender Analysis',
+      fontSize: 10
+    },
+    legend: {
+      position : 'bottom',
+      labels:{
+        usePointStyle:true,
+        pointStyle: 'circle',
+        fontSize:8
+
+      }
+    },
+  }
+});
+
+
+
+
+$(document).ready(function(){
+
+
+            $('.theme-toggle').click(function(){
+                var element = document.body;  
+                var url = $('#mode').attr('src');       
+                element.classList.toggle("dark");
+                $('header').toggleClass('header-dark');
+                $('.item').toggleClass('item-dark');
+
+                if(url=="moon.png")
+                {
+                dark="false";
+                $('#mode').attr('src','sun.png');
+                }
+                else if(url=="sun.png")
+                {
+                dark="true";
+                $('#mode').attr('src','moon.png');
+                }
+           
+            });
+        }); 
+
+
+
+
+    </script>
+
+</body>
+</html>
